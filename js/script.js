6cbcf84e2ca3d17f7f6a661b91b7e3965697e155
kxalot <r.gilcantos@Gmail.com>
2025-06-21 20:17:13 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+function startGame() {
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("game-screen").style.display = "block";
+  document.getElementById("game-screen").innerHTML = "<h2>Comienza el juego... 🔥</h2>";
+}
